aed161c47eb21c9b242d1f0a22a2e2e0b78266cc
Sachi <mbsachidanand@gmail.com>
2025-09-08 22:21:01 +0530

    Revert GraphicalDatePickerStyle - it messed up the page layout
    - Revert back to CompactDatePickerStyle()
    - GraphicalDatePickerStyle caused layout issues
    - Restore original compact date picker functionality
    - Keep date picker working as before

--- Killshot/ContentView.swift
+++ Killshot/ContentView.swift
@@ -447,7 +447,7 @@ struct AddExpenseView: View {
                 .foregroundColor(.primary)
             
             DatePicker("", selection: $when, displayedComponents: .date)
-                .datePickerStyle(GraphicalDatePickerStyle())
+                .datePickerStyle(CompactDatePickerStyle())
                 .labelsHidden()
                 .padding(.horizontal, 16)
                 .padding(.vertical, 16)
